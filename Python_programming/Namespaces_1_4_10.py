d3e480e32aeda20080666f20834db74192e3af23
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-07-30 21:39:11 +0300

    + Попытка реализации программы для реализации эмуляции работы с пространствами имен

---
+++ Python_programming/Namespaces_1_4_10.py
@@ -0,0 +1,31 @@
+
+
+n = int(input())
+
+# for i in range(n):
+#     cmd, namesp, arg = input().split()
+
+scopes = {'global': {'parent': None, 'variables': set()}}
+
+def create(namespace_new, parent):
+    if parent in scopes['global']:
+        scopes['global'] = 123
+
+create('nw_nspc', 'parent')
+
+print(scopes)
+
+# def add(namespace, var):
+    # namespace[]
+
+
+
+
+
+
+
+
+
+
+
+
