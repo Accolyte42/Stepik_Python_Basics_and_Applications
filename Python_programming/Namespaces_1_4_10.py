bea4e0bbc1203ed56dd29d56e950b0afbe55bc4a
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-08-01 23:43:50 +0300

    + Сделал вывод для отсутствующих элементов + Программа наконец-то работает=)

--- Python_programming/Namespaces_1_4_10.py
+++ Python_programming/Namespaces_1_4_10.py
@@ -1,11 +1,13 @@
-#
-#
-#
-#
-#
-#
-#
-# g
+# Реализуйте программу, которая будет эмулировать работу с пространствами
+# имен. Необходимо реализовать поддержку создания пространств имен и
+# добавление в них переменных.
+# В данной задаче у каждого пространства имен есть уникальный текстовый
+# идентификатор – его имя.
+# Вашей программе на вход подаются следующие запросы:
+# create <namespace> <parent> –  создать новое пространство имен с именем <namespace> внутри пространства <parent>
+# add <namespace> <var> – добавить в пространство <namespace> переменную <var>
+# get <namespace> <var> – получить имя пространства, из которого будет взята переменная <var>
+# при запросе из пространства <namespace>, или None, если такого пространства не существует
 
 # Идея решения: создать два словаря вида {Родитель: неймспейс } и {Неймспейс: его переменные}
 #
@@ -13,27 +15,32 @@
 #
 
 def add(namespace, var):
+    # Функция добавления переменной var в пространства имен namespace
     global dct_parent, dct_variables
     dct_variables[namespace].append(var)
 
 
 def create(namespace, parent):
+    # Функция создания пространства имен namespace в другом parent
     global dct_parent, dct_variables
     dct_parent[namespace] = parent
     dct_variables[namespace] = []
 
 
 def get(namespace, var):
+    # Функция получения переменной var в пространстве имен namespace
     if var in dct_variables[namespace]:
         print(namespace)
         return namespace
+    elif namespace == 'global':
+        print('None')
+        return 'None'
     else:
-        if namespace == 'global':
-            return 'None'
         get(dct_parent[namespace], var)
 
 
 def command(cmd_in, namesp_in, arg_in):
+    # Функция для применения команд по введенным переменным
     if cmd_in == 'add':
         return add(namesp_in, arg_in)
     if cmd_in == 'create':
@@ -45,14 +52,24 @@ def command(cmd_in, namesp_in, arg_in):
 dct_parent = {'None':'global'}
 dct_variables = {'global':['test']}
 
-# n = int(input())
-n  = 9
+# add('global', 'a')
+# create('foo', 'global')
+# add('foo', 'b')
+# get('foo', 'a')
+# get('foo', 'c')
+# create('bar', 'foo')
+# add('bar', 'a')
+# get('bar', 'a')
+# get('bar', 'b')
+# n  = 9
+
+n = int(input())
 for i in range(n):
-   cmd, namesp, arg = input().split()
-   command(cmd, namesp, arg)
-   print(cmd, namesp, arg)
-   print(dct_parent)
-   print(dct_variables)
+    cmd, namesp, arg = input().split()
+    command(cmd, namesp, arg)
+   # print(cmd, namesp, arg)
+   # print(dct_parent)
+   # print(dct_variables)
 
 
 # create('new_nmsp', 'global')
@@ -69,8 +86,8 @@ for i in range(n):
 # add('fun',)
 # print(get('foo', 'a'))
 
-print(dct_parent)
-print(dct_variables)
+# print(dct_parent)
+# print(dct_variables)
 
 
 
