0fd84552a10dd85f9b9a60b2e999ed204a06651d
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-08-01 21:41:48 +0300

    + Сделал два словаря со связями и значениями + Добавил все функции - Не работает вариант вывод переменной из global

--- Python_programming/Namespaces_1_4_10.py
+++ Python_programming/Namespaces_1_4_10.py
@@ -1,24 +1,76 @@
+#
+#
+#
+#
+#
+#
+#
+# g
 
-
-n = int(input())
-
-# for i in range(n):
-#     cmd, namesp, arg = input().split()
-
-scopes = {'global': {'parent': None, 'variables': set()}}
-
-def create(namespace_new, parent):
-    if parent in scopes['global']:
-        scopes['global'] = 123
-
-create('nw_nspc', 'parent')
-
-print(scopes)
+# Идея решения: создать два словаря вида {Родитель: неймспейс } и {Неймспейс: его переменные}
+#
+#
+#
 
 def add(namespace, var):
-    namespace[]
+    global dct_parent, dct_variables
+    dct_variables[namespace].append(var)
 
 
+def create(namespace, parent):
+    global dct_parent, dct_variables
+    dct_parent[namespace] = parent
+    dct_variables[namespace] = []
+
+
+def get(namespace, var):
+    if var in dct_variables[namespace]:
+        print(namespace)
+        return namespace
+    else:
+        if namespace == 'global':
+            return 'None'
+        get(dct_parent[namespace], var)
+
+
+def command(cmd_in, namesp_in, arg_in):
+    if cmd_in == 'add':
+        return add(namesp_in, arg_in)
+    if cmd_in == 'create':
+        return create(namesp_in, arg_in)
+    if cmd_in == 'get':
+        return get(namesp_in, arg_in)
+
+# Создание первых элементов
+dct_parent = {'None':'global'}
+dct_variables = {'global':['test']}
+
+# n = int(input())
+n  = 9
+for i in range(n):
+   cmd, namesp, arg = input().split()
+   command(cmd, namesp, arg)
+   print(cmd, namesp, arg)
+   print(dct_parent)
+   print(dct_variables)
+
+
+# create('new_nmsp', 'global')
+# create('new_nmsp', 'global')
+# add('global', 'var11')
+# add('new_nmsp', 'var22')
+
+# add('global', 'a')
+# create('foo', 'global')
+# add('foo', 'b')
+# create('bar', 'foo')
+# add('bar', 'a')
+# create('fun', 'foo')
+# add('fun',)
+# print(get('foo', 'a'))
+
+print(dct_parent)
+print(dct_variables)
 
 
 
