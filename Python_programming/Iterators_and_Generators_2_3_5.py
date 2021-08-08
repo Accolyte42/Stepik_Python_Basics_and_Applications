5373b88be32900c38a345ee979750a69c280f85a
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-08-09 00:01:50 +0300

    + Реализация вывода всех простых чисел меньше заданного. + Простота определяется в генераторе с помощьью факториала (формула Вильсона)

--- Python_programming/Iterators_and_Generators_2_3_5.py
+++ Python_programming/Iterators_and_Generators_2_3_5.py
@@ -1,6 +1,7 @@
 
 
 from itertools import takewhile
+from math import factorial
 import itertools
 
 
@@ -9,14 +10,15 @@ def primes():
     while True:
         a += 1
         # print(a, end=' ')
-        # if fac(a-1)+1 % a == 0:
-        # for i in
-        yield a
-
+        test_fac = factorial(a-1)
+        test_123 = test_fac+1
+        test = (test_123 % a == 0)
+        if test:
+            yield a
 
 # print(list(itertools.takewhile(lambda x: x <= 31, [1,2,3,4,10])))
 
-print(list(itertools.takewhile(lambda x: x <= 10, primes())))
+print(list(itertools.takewhile(lambda x: x <= 41, primes())))
 
 
 
