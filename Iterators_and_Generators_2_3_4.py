039053644b42fcecac1871eb5b65852625ab2609
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-08-08 22:36:44 +0300

    НАКОНЕЦ_ТО + СДелал правильный __next__ (с yield не работало) + добавил описнание задания

--- Iterators_and_Generators_2_3_4.py
+++ Iterators_and_Generators_2_3_4.py
@@ -1,3 +1,19 @@
+# Одним из самых часто используемых классов в Python является класс filter. Он
+# принимает в конструкторе два аргумента a и f – последовательность и функцию,
+# и позволяет проитерироваться только по таким элементам x из
+# последовательности a, что f(x) равно True. Будем говорить, что в этом случае
+# функция f допускает элемент x, а элемент x является допущенным.
+#
+# В данной задаче мы просим вас реализовать класс multifilter, который будет
+# выполнять ту же функцию, что и стандартный класс filter, но будет использовать
+# не одну функцию, а несколько.
+#
+# Решение о допуске элемента будет приниматься на основании того, сколько
+# функций допускают этот элемент, и сколько не допускают. Обозначим эти количества за pos и neg.
+#
+# Введем понятие решающей функции – это функция, которая принимает два
+# аргумента – количества pos и neg, и возвращает True, если элемент допущен, и False иначе.
+
 
 class multifilter:
     def judge_half(pos, neg):
@@ -7,7 +23,7 @@ class multifilter:
             return False
 
     def judge_all(pos, neg):
-        print('judge_all')
+        # print('judge_all')
         if neg == 0:
             return True
         else:
@@ -19,36 +35,37 @@ class multifilter:
         else:
             return False
 
-    def __init__(self, iterable, *funcs, judge=judge_all):
+    def __iter__(self):
+        return self
+
+    def __init__(self, iterable, *funcs, judge=judge_any):
         self.iterable = iterable
         self.judge = judge
         self.funcs = funcs
         self.index = 0
-        print('init')
+        # print('init')
         # print(self.funcs)
-
-    def __iter__(self):
-        return self
 
     def __next__(self):
         # print(len(self.iterable))
-        print('next')
+        # print()
         if self.index < len(self.iterable):
             self.index += 1
+            # return self.index
             pos = 0
             neg = 0
             for f in self.funcs:
-                if f(self.index):
+                if f(self.index-1):
                     pos += 1
                 else:
                     neg += 1
-            print(self.judge(pos, neg))
+            # print(self.iterable[self.index-1], self.judge(pos, neg))
             if self.judge(pos, neg):
-                # print(self.iterable[self.index])
-                yield self.iterable[self.index]
+                return self.iterable[self.index-1]
             else:
                 return self.__next__()
-        raise StopIteration
+        else:
+            raise StopIteration
 
 #             yield self.iterable[i]
 
@@ -66,8 +83,18 @@ def fun5(x):
 
 
 a = [i for i in range(21)]
-print(multifilter(a, fun2, fun3, fun5))
-# ob = multifilter(a)
-for i in multifilter(a, fun2, fun3, fun5):
-    print(i)
+# print(multifilter(a, fun2, fun3, fun5))
+ob = multifilter(a, fun2, fun3, fun5, judge=multifilter.judge_half)
+# print(ob)
+# for i in
+# for i in multifilter(a, fun2, fun3, fun5):
+#     print(i)
     # print('TEST')
+
+# for i in multifilter(a, fun2, fun3, fun5):
+#     print(i, end=' ')
+print(list(ob))
+
+
+
+
