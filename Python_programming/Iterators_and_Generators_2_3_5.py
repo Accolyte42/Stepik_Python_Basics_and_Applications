9f75440628fe7b7dc2eb773a5aea17ad439b5390
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-08-09 18:59:30 +0300

    + Чтение из файла и вывод в файл строки в обратном порядке

--- Python_programming/Iterators_and_Generators_2_3_5.py
+++ Python_programming/Iterators_and_Generators_2_3_5.py
@@ -9,11 +9,7 @@ def primes():
     a = 1
     while True:
         a += 1
-        # print(a, end=' ')
-        test_fac = factorial(a-1)
-        test_123 = test_fac+1
-        test = (test_123 % a == 0)
-        if test:
+        if (factorial(a-1)+1) % a == 0:
             yield a
 
 # print(list(itertools.takewhile(lambda x: x <= 31, [1,2,3,4,10])))
