4749978e0033b555761731071bd690af7367ea2c
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-08-12 22:34:35 +0300

    + Считывание строк и вывод тех из них, в которых есть ровно три символа между z и z

--- Python_programming/Regular_expressions_3_2_8.py
+++ Python_programming/Regular_expressions_3_2_8.py
@@ -1,10 +1,10 @@
 import sys, re
 
-pattern = r'\bcat\b'
+pattern = r'z.{3}z'
 #
 # string = 'cat2 catcc gcat cat'
 # print(re.search(pattern, string))
-counter = 0
+# counter = 0
 for line in sys.stdin:
     line = line.rstrip()
     if re.search(pattern, line) is not None:
