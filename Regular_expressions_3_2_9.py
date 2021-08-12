4d85cdbf617bdd4636afca681a677f8b9ca8169a
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-08-12 23:19:40 +0300

    + Считывание строк и вывод тех из них, в которых есть символ \

---
+++ Regular_expressions_3_2_9.py
@@ -0,0 +1,15 @@
+import sys, re
+
+pattern = r'\\'
+#
+# string = 'cat2 catcc gcat cat'
+# print(re.search(pattern, string))
+# counter = 0
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.search(pattern, line) is not None:
+        print(line)
+
+
+
+
