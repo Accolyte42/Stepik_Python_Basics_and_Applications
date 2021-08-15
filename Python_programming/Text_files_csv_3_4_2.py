d0f000ee74f1e69ad62c8ce541a39422fa5a00e8
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-08-16 00:04:02 +0300

    + добавил поиск по всем парам в словаре преступления, нашел самое распространненое + Сделал вывод этого преступления и его количество

--- Python_programming/Text_files_csv_3_4_2.py
+++ Python_programming/Text_files_csv_3_4_2.py
@@ -4,9 +4,6 @@
 # Вам необходимо узнать тип преступления, которое было зафиксировано максимальное число раз в 2015 году.
 
 
-
-
-
 import csv
 from datetime import datetime, date, time
 
@@ -14,40 +11,21 @@ from datetime import datetime, date, time
 dict_crimes = {}
 with open('Crimes.csv') as f:
     reader = csv.DictReader(f)
-    # print(reader.__sizeof__())
-    # print(reader)
     for row in reader:
-        # print(row[2][0:-3])
-        # dt = row[2]
         dt = datetime.strptime(row['Date'][:10], '%m/%d/%Y')
-        # print(repr(row['Date']))
-        # print(repr(dt))
-
-        # print(repr(dt.year)) # Обращение к году преступления
-        # print(repr(row['Primary Type']))
         if dt.year == 2015:
-            # dict_crimes[row['Primary Type']] = 1
             try:
                 dict_crimes[row['Primary Type']] += 1
             except:
                 dict_crimes[row['Primary Type']] = 1
-        # if dt:
-        #     new_dt = datetime.strptime(dt, '%m/%d/%Y %H:%M:%S %p')
-        #     print(repr(datetime.strptime(dt, '%m/%d/%Y %H:%M:%S %p')))
-        # print(repr(new_dt))
-print(dict_crimes)
 
+print(dict_crimes) # вывод всего списка с перступлениями и их количеством
+dct_it = dict_crimes.items()
+num_of_crimes = 0
+most_common_crime = 'Не найдено'
+for crime in dct_it:
+    if crime[1] > num_of_crimes:
+        num_of_crimes = crime[1]
+        most_common_crime = crime[0]
 
-# students = [
-#     ['Greg', 'Dean', 70, 80, 90, 'Good job, Greg'],
-#     ['Wirt', 'Wood', 80, 80.2, 80, 'Nicely done']
-# ]
-#
-# with open('example_csv_format', 'a') as f:
-#     # writer = csv.writer(f, quoting=csv.QUOTE_ALL) # в аргументе quoting можно выбирать, что помещать в ковычки
-#     writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC) # в аргументе quoting можно выбирать, что помещать в ковычки
-#
-#     # for student in students:
-#     #     writer.writerow(student)
-#     writer.writerows(students) # Данная запись экспивалентна, т.к.
-#                                # интерпретатор умеет итерировать
+print(most_common_crime, num_of_crimes)
