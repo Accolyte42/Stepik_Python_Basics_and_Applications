818507d993eadc0ce93c4073ec3333591c1dbca6
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-08-15 22:42:25 +0300

    + Запись данных в файл в формате csv. С заключением или незаключением значений в ковычки (quoting)

--- Python_programming/Text_files_csv_json_3_4_2.py
+++ Python_programming/Text_files_csv_json_3_4_2.py
@@ -1,8 +1,21 @@
 import csv
 
-with open('example_csv_format') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        if row:
-            print(row)
+# with open('example_csv_format') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         if row:
+#             print(row)
 
+students = [
+    ['Greg', 'Dean', 70, 80, 90, 'Good job, Greg'],
+    ['Wirt', 'Wood', 80, 80.2, 80, 'Nicely done']
+]
+
+with open('example_csv_format', 'a') as f:
+    # writer = csv.writer(f, quoting=csv.QUOTE_ALL) # в аргументе quoting можно выбирать, что помещать в ковычки
+    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC) # в аргументе quoting можно выбирать, что помещать в ковычки
+
+    # for student in students:
+    #     writer.writerow(student)
+    writer.writerows(students) # Данная запись экспивалентна, т.к.
+                               # интерпретатор умеет итерировать
